52ba563c3e69a482ee2d26ed30c91c7958de0081
Guilherme Chaves <gb.chaves@hotmail.com>
2023-12-03 22:20:12 -0300

    atualização da conexão DAO

--- src/model/dao/Conexao.java
+++ src/model/dao/Conexao.java
@@ -8,7 +8,7 @@ public class Conexao {
 
     private static final String url = "jdbc:mysql://localhost:3306/projeto_asw_poo";
     private static final String user = "root";
-    private static final String password = "root";
+    private static final String password = "aincrad";
 
     private static Connection conn = null;
 
